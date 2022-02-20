04c3b9fc1f3012c10a0d0ba495b79f9f98b298e7
Einar Ingebrigtsen <einari@me.com>
2022-02-20 10:08:45 +0100

    Adding tracking events for reading, stopping and settings

--- Source/Extension/browser/index.ts
+++ Source/Extension/browser/index.ts
@@ -1,4 +1,5 @@
 import { Noise } from './Noise';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
 
 const container = document.createElement('div');
@@ -23,7 +24,28 @@ canvas.style.opacity = '0.5';
 container.appendChild(canvas);
 
 
+function createUserToken() {
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    return hex;
+}
+let userId: string = '';
+
+const appInsights = new ApplicationInsights({
+    config: {
+        connectionString: 'InstrumentationKey=811364f1-6066-4154-adc1-97b0ebd65535;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/'
+    }
+});
+appInsights.loadAppInsights();
+
 const noise = new Noise(canvas);
+window.onbeforeunload = function (e) {
+    appInsights.trackEvent({ name: 'stopReading' }, { userId })
+};
 
 function handleOpacity(opacity: number | undefined = 0.3) {
     canvas.style.opacity = opacity?.toString();
@@ -31,18 +53,36 @@ function handleOpacity(opacity: number | undefined = 0.3) {
 
 handleOpacity()
 
-chrome.storage.sync.get(['milliseconds', 'opacity'], result => {
-    if (result.milliseconds) {
-        noise.milliseconds = result.milliseconds;
+function handleEnabled(enabled: boolean) {
+    canvas.style.display = enabled ? '' : 'none';
+}
+
+chrome.storage.sync.get(['milliseconds', 'opacity', 'enabled', 'userId'], items => {
+    if (items.enabled !== undefined) {
+        handleEnabled(items.enabled);
+    }
+    if (items.milliseconds) {
+        noise.milliseconds = items.milliseconds;
     }
 
-    if (result.opacity) {
-        handleOpacity(result.opacity);
+    if (items.opacity) {
+        handleOpacity(items.opacity);
     }
+
+    userId = items.userId;
+    if (!userId || userId == '') {
+        userId = createUserToken();
+        chrome.storage.sync.set({ userId }, function () {
+        });
+    }
+
+    appInsights.trackEvent({ name: 'startReading' }, { userId, milliseconds: items.milliseconds, opacity: items.opacity });
 });
 
 chrome.storage.onChanged.addListener(changes => {
-    console.log(changes);
+    if (changes.enabled) {
+        handleEnabled(changes.enabled.newValue);
+    }
     if (changes.milliseconds) {
         noise.milliseconds = changes.milliseconds.newValue;
     }
--- Source/Extension/configuration/App.tsx
+++ Source/Extension/configuration/App.tsx
@@ -1,30 +1,63 @@
-import { Stack, Slider } from '@fluentui/react';
+import { Checkbox, Stack, Slider } from '@fluentui/react';
 import { useState, useEffect } from 'react';
 import { default as styles } from './App.module.scss';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
 const defaultMilliseconds = 60;
 const defaultOpacity = 0.3;
 
+function createUserToken() {
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    return hex;
+}
+let userId: string = '';
+
+const appInsights = new ApplicationInsights({
+    config: {
+        connectionString: 'InstrumentationKey=811364f1-6066-4154-adc1-97b0ebd65535;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/'
+    }
+});
+appInsights.loadAppInsights();
+
+
 export const App = () => {
+    const [enabled, setEnabled] = useState(true);
     const [milliseconds, setMilliseconds] = useState(100);
     const [opacity, setOpacity] = useState(0.5);
 
     useEffect(() => {
-        chrome.storage.sync.get(['milliseconds', 'opacity'], result => {
-            if (!result.milliseconds) {
+        chrome.storage.sync.get(['milliseconds', 'opacity', 'enabled', 'userId'], items => {
+            if (items.enabled !== undefined) {
+                setEnabled(items.enabled);
+            }
+
+            if (!items.milliseconds) {
                 chrome.storage.sync.set({ milliseconds: defaultMilliseconds }, () => { });
                 setMilliseconds(defaultMilliseconds);
             } else {
-                setMilliseconds(result.milliseconds);
+                setMilliseconds(items.milliseconds);
             }
 
-            if (!result.opacity) {
+            if (!items.opacity) {
                 chrome.storage.sync.set({ opacity: defaultOpacity }, () => { });
                 setOpacity(defaultOpacity);
             } else {
-                setOpacity(result.opacity);
+                setOpacity(items.opacity);
             }
 
+            userId = items.userId;
+            if (!userId || userId == '') {
+                userId = createUserToken();
+                chrome.storage.sync.set({ userId }, function () {
+                });
+            }
+
+            appInsights.trackEvent({ name: 'settings' }, { userId, milliseconds, opacity });
         });
 
         return () => { };
@@ -33,6 +66,15 @@ export const App = () => {
     return (
         <>
             <Stack className={styles.configuration}>
+                <Checkbox
+                    label="Enabled"
+                    checked={enabled}
+                    onChange={(_, value) => {
+                        chrome.storage.sync.set({ enabled: value }, () => { })
+                        setEnabled(value);
+                    }}
+                />
+
                 <Slider
                     label="Interval (MS)"
                     min={20}
@@ -44,6 +86,7 @@ export const App = () => {
                     onChange={(value) => {
                         chrome.storage.sync.set({ milliseconds: value }, () => { });
                         setMilliseconds(value);
+                        appInsights.trackEvent({ name: 'settings' }, { userId, milliseconds });
                     }} />
 
                 <Slider
@@ -56,6 +99,7 @@ export const App = () => {
                     onChange={(value) => {
                         chrome.storage.sync.set({ opacity: value }, () => { });
                         setOpacity(value);
+                        appInsights.trackEvent({ name: 'settings' }, { userId, opacity });
                     }} />
             </Stack>
         </>
